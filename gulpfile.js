37a9be86728e946e60a185ee8ea1c2c211d336be
Ross Weinstein <Pamela@Pamelas-iMac.local>
2018-06-16 08:17:52 -0500

    wrote gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync');
+
+gulp.task('dev', ['html', 'stylesheets', 'js'], function () {
+    browserSync.init({
+        server: './dist'
+    });
+
+    gulp.watch('src/js/*.js', ['js-watch']);
+    gulp.watch('src/scss/*.scss', ['stylesheets']);
+    gulp.watch('src/html/*.html', ['html']).on('change', browserSync.reload);
+});
+
+gulp.task('html', () =>
+    gulp.src('src/html/*.html')
+        .pipe(gulp.dest('dist/html'))
+);
+
+gulp.task('stylesheets', () =>
+    gulp.src('src/scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream())
+);
+
+gulp.task('js-watch', ['js'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('js', function () {
+    return gulp.src('src/js/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('default', ['dev']);
