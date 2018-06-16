e770057186e5458ac4663510be17f50e29652383
Ross Weinstein <ross.a.weinstein@gmail.com>
2018-06-16 09:01:15 -0500

    small debug

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,35 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
-const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync');
 const imagemin = require('gulp-imagemin');
 
-gulp.task('dev', ['html', 'stylesheets', 'js', 'images'], function () {
+gulp.task('dev', ['html', 'css', 'js', 'images'], function () {
     browserSync.init({
         server: './dist'
     });
 
     gulp.watch('src/images/*', ['images']);
     gulp.watch('src/js/*.js', ['js-watch']);
-    gulp.watch('src/scss/*.scss', ['stylesheets']);
+    gulp.watch('src/scss/*.scss', ['css']);
     gulp.watch('src/html/*.html', ['html']).on('change', browserSync.reload);
 });
 
-gulp.task('html', () =>
+gulp.task('html', function () {
     gulp.src('src/html/*.html')
         .pipe(gulp.dest('dist/html'))
-);
+});
 
-gulp.task('stylesheets', () =>
-    gulp.src('src/scss/*.scss')
-        .pipe(sass())
+gulp.task('css', function () {
+    return gulp.src('src/css/*.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
         .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream())
-);
+});
 
 gulp.task('js-watch', ['js'], function (done) {
     browserSync.reload();
