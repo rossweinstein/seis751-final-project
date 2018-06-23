a5bbca6654582cbf01299a482221cac355f20f59
Ross Weinstein <ross.a.weinstein@gmail.com>
2018-06-23 16:19:40 -0500

    making improvements

--- src/js/begin.js
+++ src/js/begin.js
@@ -1,4 +1,69 @@
-const birdImage = document.getElementById('tree-swallow');
-birdImage.addEventListener('click', () => {
-    console.log('Tree Swallow Clicked');
-}, false);
+var belterKingfisher = document.getElementById("belted-kingfisher");
+
+belterKingfisher.addEventListener('mouseover', function() {
+  document.getElementById('bird-name').innerHTML = "Belted Kingfisher";
+})
+
+belterKingfisher.addEventListener('mouseout', function() {
+  document.getElementById('bird-name').innerHTML = "";
+})
+
+var barnSwallow = document.getElementById("barn-swallow");
+
+barnSwallow.addEventListener('mouseover', function() {
+  document.getElementById('bird-name').innerHTML = "Barn Swallow";
+})
+
+barnSwallow.addEventListener('mouseout', function() {
+  document.getElementById('bird-name').innerHTML = "";
+})
+
+var treeSwallow = document.getElementById("tree-swallow");
+
+treeSwallow.addEventListener('mouseover', function() {
+  document.getElementById('bird-name').innerHTML = "Tree Swallow";
+})
+
+treeSwallow.addEventListener('mouseout', function() {
+  document.getElementById('bird-name').innerHTML = "";
+})
+
+var blueJay = document.getElementById("blue-jay");
+
+blueJay.addEventListener('mouseover', function() {
+  document.getElementById('bird-name').innerHTML = "Blue Jay";
+})
+
+blueJay.addEventListener('mouseout', function() {
+  document.getElementById('bird-name').innerHTML = "";
+})
+
+var indigoBunting = document.getElementById("indigo-bunting");
+
+indigoBunting.addEventListener('mouseover', function() {
+  document.getElementById('bird-name').innerHTML = "Indigo Bunting";
+})
+
+indigoBunting.addEventListener('mouseout', function() {
+  document.getElementById('bird-name').innerHTML = "";
+})
+
+var purpleMartin = document.getElementById("purple-martin");
+
+purpleMartin.addEventListener('mouseover', function() {
+  document.getElementById('bird-name').innerHTML = "Purple Martin";
+})
+
+purpleMartin.addEventListener('mouseout', function() {
+  document.getElementById('bird-name').innerHTML = "";
+})
+
+var easternBluebird = document.getElementById("eastern-bluebird");
+
+easternBluebird.addEventListener('mouseover', function() {
+  document.getElementById('bird-name').innerHTML = "Eastern Bluebird";
+})
+
+easternBluebird.addEventListener('mouseout', function() {
+  document.getElementById('bird-name').innerHTML = "";
+})
